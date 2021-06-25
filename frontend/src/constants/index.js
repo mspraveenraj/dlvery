143130b8452d6703e67fef04e055195671fa0c09
Praveenraj <praveenrajpraveenraj007@gmail.com>
2021-06-25 15:17:00 +0530

    bug fix

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,7 +1,7 @@
-export const API_BASE_URL = 'https://dlvery-deployment.herokuapp.com';
+export const API_BASE_URL = 'https://dlveryapp.herokuapp.com';
 export const ACCESS_TOKEN = 'accessToken';
 
-export const OAUTH2_REDIRECT_URI = 'https://dlvery-deployment.herokuapp.com/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = 'https://dlveryapp.herokuapp.com/oauth2/redirect'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
